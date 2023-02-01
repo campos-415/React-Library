8ffd7f2842969c91804bfdc0cc17d8ff6ea019ba
Cesar Campos <camposss.415@gmail.com>
2023-02-01 05:23:29 -0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import Nav from "./components/Nav.jsx";
-import Landing from "./components/Landing.jsx";
-import Highlights from "./components/Highlights.jsx";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBars, faBolt, faBookOpen, faShoppingCart, faStar, faStarHalfAlt, faTags, faTimes } from "@fortawesome/free-solid-svg-icons";
-import Featured from "./components/Featured.jsx";
-import Discounted from "./components/Discounted.jsx";
-import Explore from "./components/Explore.jsx";
 import Footer from "./components/Footer.jsx";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Books from "./pages/Books.jsx";
+import { books } from './data'
 
 library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt )
 
 function App() {
   return (
     <>
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path='/' exact component={Home} />
+        <Route path={"/books"} render={() => <Books book={books} />} />
+        <Footer />
+      </div>
+    </Router>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Discounted from '../components/Discounted'
+import Explore from '../components/Explore'
+import Featured from '../components/Featured'
+import Highlights from '../components/Highlights'
+import Landing from '../components/Landing'
+
+const Home = () => {
+  return (
+    <>
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+    </>
+  )
+}
+
+export default Home
